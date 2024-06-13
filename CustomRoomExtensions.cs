e188b90e3e815c24956a3fb8f1e52d0565cb174b
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-06-13 12:40:13 -0300

    Adicionar arquivos de projeto.

---
+++ CustomRoomExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace EditorCustomRooms
+{
+	public static class CustomRoomExtensions
+	{
+		public static RoomAsset GetAssetFromPath(string path)
+		{
+			return null; // placeholder
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace EditorCustomRooms
+{
+    [BepInPlugin("pixelguy.pixelmodding.baldiplus.editorcustomrooms", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+			new Harmony("pixelguy.pixelmodding.baldiplus.editorcustomrooms").PatchAll();
+
+
+        }
+    }
+}
