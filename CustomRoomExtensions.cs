73ed45f5834919a625efeb648609c2665e866e0e
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-06-14 13:09:24 -0300

    Custom rooms done

--- CustomRoomExtensions.cs
+++ CustomRoomExtensions.cs
@@ -1,12 +1,139 @@
-﻿using UnityEngine;
+﻿using PlusLevelLoader;
+using PlusLevelFormat;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using MTM101BaldAPI;
 
 namespace EditorCustomRooms
 {
 	public static class CustomRoomExtensions
 	{
-		public static RoomAsset GetAssetFromPath(string path)
+		public static RoomAsset GetAssetFromPath(string path, int spawnWeight, bool allCellsAreLightCells, Transform lightPre, int minItemValue, int maxItemValue, bool isOffLimits, RoomFunctionContainer existingContainer)
 		{
-			return null; // placeholder
+			if (!File.Exists(path) || Path.GetExtension(path) != ".cbld")
+				throw new System.ArgumentException($"Path ({path}) is invalid! It must be a .cbld file!");
+
+			using BinaryReader reader = new(File.OpenRead(path));
+
+			var lvlAsset = CustomLevelLoader.LoadLevelAsset(LevelExtensions.ReadLevel(reader));
+			RoomAsset rAsset = ScriptableObject.CreateInstance<RoomAsset>();
+
+
+			// **************** Load Room Asset Data **************
+			// Note, everything must be stored as value, not reference; LevelAssets will be destroyed afterwards
+			// There should be only a single room per level asset (only uses rooms[1])
+
+			rAsset.activity = lvlAsset.rooms[1].activity.GetNew();
+			rAsset.basicObjects = new(lvlAsset.rooms[1].basicObjects);
+			rAsset.blockedWallCells = new(lvlAsset.rooms[1].blockedWallCells);
+			rAsset.category = lvlAsset.rooms[1].category;
+
+			foreach (var cell in lvlAsset.tile)
+			{
+				if (cell.type < 16 && cell.roomId == 1)
+					rAsset.cells.Add(cell);
+			}
+
+			rAsset.color = lvlAsset.rooms[1].color;
+			rAsset.doorMats = lvlAsset.rooms[1].doorMats;
+			rAsset.entitySafeCells = new(lvlAsset.rooms[1].entitySafeCells);
+			rAsset.eventSafeCells = new(lvlAsset.rooms[1].eventSafeCells);
+			rAsset.forcedDoorPositions = new(lvlAsset.rooms[1].forcedDoorPositions);
+			rAsset.hasActivity = lvlAsset.rooms[1].hasActivity;
+			rAsset.itemList = new(lvlAsset.rooms[1].itemList);
+			rAsset.items = new(lvlAsset.rooms[1].items);
+			for (int i = 0; i < rAsset.basicObjects.Count; i++)
+			{
+				var obj = rAsset.basicObjects[i];
+				if (obj.prefab.name == "itemSpawnMarker")
+				{
+					rAsset.basicObjects.RemoveAt(i--);
+					rAsset.itemSpawnPoints.Add(new() { weight = 50, position = new(obj.position.x, obj.position.z) });
+				}
+			}
+
+			//rAsset.itemSpawnPoints = new(lvlAsset.rooms[1].itemSpawnPoints);
+			rAsset.keepTextures = false;
+			rAsset.lightPre = lightPre;
+			rAsset.mapMaterial = lvlAsset.rooms[1].mapMaterial;
+			rAsset.maxItemValue = maxItemValue;
+			rAsset.minItemValue = minItemValue;
+			rAsset.offLimits = isOffLimits;
+
+			for (int i = 0; i < rAsset.basicObjects.Count; i++)
+			{
+				var obj = rAsset.basicObjects[i];
+				if (obj.prefab.name == "potentialDoorMarker")
+				{
+					rAsset.basicObjects.RemoveAt(i--);
+					rAsset.potentialDoorPositions.Add(IntVector2.GetGridPosition(obj.position));
+				}
+			}
+
+			//rAsset.potentialDoorPositions = new(lvlAsset.rooms[1].potentialDoorPositions); // Check if this works
+			rAsset.requiredDoorPositions = new(lvlAsset.rooms[1].requiredDoorPositions);
+			rAsset.secretCells = new(lvlAsset.rooms[1].secretCells); // don't know how should this be set up
+			rAsset.spawnWeight = spawnWeight;
+
+			if (allCellsAreLightCells)
+				rAsset.standardLightCells.AddRange(rAsset.cells.Select(x => x.pos));
+
+			rAsset.type = lvlAsset.rooms[1].type;
+
+			rAsset.name = $"Room_{rAsset.category}_{Path.GetFileNameWithoutExtension(path)}";
+			Object.SetName(rAsset, rAsset.name); // Workaround to the stupid " public new string name = "" "
+
+			if (existingContainer)
+				rAsset.roomFunctionContainer = existingContainer;
+			else
+			{
+				var roomFunctionContainer = new GameObject(rAsset.name + "FunctionContainer").AddComponent<RoomFunctionContainer>();
+				roomFunctionContainer.gameObject.ConvertToPrefab(true);
+
+				rAsset.roomFunctionContainer = roomFunctionContainer;
+			}
+
+
+			Object.Destroy(lvlAsset); // Remove the created level asset from memory
+			return rAsset;
 		}
+
+		public static RoomAsset SetPotentialPosters(this RoomAsset asset, float posterChance, params WeightedPosterObject[] posters)
+		{
+			asset.posterChance = posterChance;
+			asset.posters = new(posters);
+			return asset;
+		}
+
+		public static RoomAsset SetPotentialWindows(this RoomAsset asset, float windowChance, WindowObject window)
+		{
+			asset.windowChance = windowChance;
+			asset.windowObject = window;
+			return asset;
+		}
+
+		public static RoomAsset SetMats(this RoomAsset asset, Material ceil, Material wall, Material floor)
+		{
+			if (ceil)
+			{
+				asset.ceilMat = new(ceil);
+				asset.ceilTex = Object.Instantiate((Texture2D)ceil.mainTexture);
+			}
+			if (wall)
+			{
+				asset.wallMat = new(wall);
+				asset.wallTex = Object.Instantiate((Texture2D)wall.mainTexture);
+			}
+			if (floor)
+			{
+				asset.florMat = new(floor);
+				asset.florTex = Object.Instantiate((Texture2D)floor.mainTexture);
+			}
+			asset.keepTextures = asset.florMat && asset.wallMat && asset.ceilMat;
+			return asset;
+		}
+
+
 	}
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,106 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System.IO;
+using System.Collections.Generic;
+using MTM101BaldAPI.AssetTools;
+using MTM101BaldAPI.Registers;
+using UnityEngine;
+using System.Linq;
+using MTM101BaldAPI;
+using BaldiLevelEditor;
+using PlusLevelFormat;
+using BepInEx.Bootstrap;
+using PlusLevelLoader;
 
 namespace EditorCustomRooms
 {
     [BepInPlugin("pixelguy.pixelmodding.baldiplus.editorcustomrooms", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+	[BepInDependency("mtm101.rulerp.baldiplus.leveleditor", BepInDependency.DependencyFlags.SoftDependency)] // soft!!
+	[BepInDependency("mtm101.rulerp.bbplus.baldidevapi", BepInDependency.DependencyFlags.HardDependency)]
+	[BepInDependency("mtm101.rulerp.baldiplus.levelloader", BepInDependency.DependencyFlags.HardDependency)]
+	public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
-			new Harmony("pixelguy.pixelmodding.baldiplus.editorcustomrooms").PatchAll();
+			new Harmony("pixelguy.pixelmodding.baldiplus.editorcustomrooms").PatchAllConditionals();
+			ModPath = AssetLoader.GetModPath(this);
 
+			LoadingEvents.RegisterOnAssetsLoaded(Info, () =>
+			{
+				if (Chainloader.PluginInfos.ContainsKey("mtm101.rulerp.baldiplus.leveleditor"))
+					AddEditorfeatures();
 
+				var lightPre = Resources.FindObjectsOfTypeAll<RoomAsset>().First(x => x.category == RoomCategory.Class).lightPre;
+				var asset = CustomRoomExtensions.GetAssetFromPath(Path.Combine(ModPath, "classroomDemo1.cbld"), 65, true, lightPre, 35, 75, false, null);
+				asset.MarkAsNeverUnload();
+				assetsToAdd.Add(new() { selection = asset, weight = 999 });
+
+			}, false);
+
+			GeneratorManagement.Register(this, GenerationModType.Addend, (floorName, floorNum, lvlObj) =>
+			{
+				lvlObj.potentialClassRooms = lvlObj.potentialClassRooms.AddRangeToArray([.. assetsToAdd]);
+			});
         }
+
+		void AddEditorfeatures()
+		{
+			CreateCube("potentialDoorMarker", Color.blue, 5f, 0);
+			CreateCube("itemSpawnMarker", Color.red, 2f, 5f);
+
+
+			static void CreateCube(string name, Color color, float scale, float offset)
+			{
+				var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				var renderer = cube.GetComponent<MeshRenderer>();
+				renderer.material.shader = Shader.Find("Unlit/Color");
+				renderer.material.color = color;
+
+				cube.transform.localScale = Vector3.one * scale;
+				cube.name = name;
+				cube.ConvertToPrefab(true);
+				markersToAdd.Add(new(name));
+
+				BaldiLevelEditorPlugin.editorObjects.Add(EditorObjectType.CreateFromGameObject<EditorPrefab, PrefabLocation>(name, cube, Vector3.up * offset));
+				PlusLevelLoaderPlugin.Instance.prefabAliases.Add(name, cube);
+			}
+				
+			
+			
+		}
+
+		internal static string ModPath = string.Empty;
+
+		readonly List<WeightedRoomAsset> assetsToAdd = [];
+
+		readonly internal static List<RotateAndPlacePrefab> markersToAdd = [];
     }
+
+	[ConditionalPatchMod("mtm101.rulerp.baldiplus.leveleditor")]
+	[HarmonyPatch]
+	internal static class PrivateCallsInEditor
+	{
+		
+		[HarmonyPatch(typeof(BaldiLevelEditorPlugin), "SetUpUIPrefabs")] // this is called once I guess
+		[HarmonyPrefix]
+		static void AddMySprites(BaldiLevelEditorPlugin __instance)
+		{
+			AddSpriteFolderToAssetMan("UI/", 40f, Plugin.ModPath, "EditorUI");
+
+
+			void AddSpriteFolderToAssetMan(string prefix = "", float pixelsPerUnit = 40f, params string[] path) // Copypaste lmao
+			{
+				string[] paths = Directory.GetFiles(Path.Combine(path));
+				for (int i = 0; i < paths.Length; i++)
+					__instance.assetMan.Add(prefix + Path.GetFileNameWithoutExtension(paths[i]), AssetLoader.SpriteFromTexture2D(AssetLoader.TextureFromFile(paths[i]), pixelsPerUnit));
+				
+			}
+		}
+
+		[HarmonyPatch(typeof(PlusLevelEditor), "SpawnUI")]
+		[HarmonyPrefix]
+		static void AddMyCats(PlusLevelEditor __instance) =>
+			__instance.toolCats.Find(x => x.name == "utilities").tools.AddRange(Plugin.markersToAdd);
+		
+	}
 }
